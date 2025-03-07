ea716ab2424b75833688ff19207bbe1d1704768d
Sam Ansmink <samansmink@hotmail.com>
2025-03-07 14:36:44 +0100

    switch to httpfs main for testing

--- extension_config.cmake
+++ extension_config.cmake
@@ -7,6 +7,7 @@ duckdb_extension_load(aws
 )
 
 duckdb_extension_load(httpfs
-        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../duckdb_httpfs
-        INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../duckdb_httpfs/extension/httpfs/include
+        GIT_URL https://github.com/duckdb/duckdb-httpfs
+        GIT_TAG main
+        INCLUDE_DIR extension/httpfs/include
 )
